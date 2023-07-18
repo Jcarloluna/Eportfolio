ed3db1b23d74bc4920899b8c0c11c63e0ed63e76
Jcarloluna <91709785+Jcarloluna@users.noreply.github.com>
2023-07-18 18:53:02 +0800

    EP_refactor/created_data_folder

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import SkillsItem from "./SkillsItem";
-import { SKILL_LOGOS } from "./SkillLogoList";
+import { SKILL_LOGOS } from "../Data/data";
 
 export const Skills = () => {
   return (
